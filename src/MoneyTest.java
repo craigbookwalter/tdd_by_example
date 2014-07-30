6f410a939b53d83c194e0c4405a3be911933def6
unknown <CraigB@CRAIGBW7X1700.us.corp>
2014-07-30 19:35:43 -0400

    begin ch13

--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -13,18 +13,9 @@ public class MoneyTest {
 	}
 
 	@Test
-	public void testFrancMultiplication() {
-		Money five = Money.franc(5);
-		assertEquals(Money.franc(10), five.times(2));
-		assertEquals(Money.franc(15), five.times(3));
-	}
-
-	@Test
 	public void testForEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
 	
@@ -35,7 +26,11 @@ public class MoneyTest {
 	}
 	
 	@Test
-	public void testDifferentClassEquality() {
-		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));		
+	public void testSimpleAddition() {
+		Money five = Money.dollar(5);
+		Expression sum = five.plus(five);
+		Bank bank = new Bank();
+		Money reduced = bank.reduce(sum, "USD");
+		assertEquals(reduced, Money.dollar(10));
 	}
 }
